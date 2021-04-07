1f03c7e1e259ad4157f2df949a432743df0fc9ee
Ametov Ildar <ametov.ildar@gmail.com>
2021-04-08 01:11:44 +0300

    fin

--- dev/web/js/main.js
+++ dev/web/js/main.js
@@ -1,37 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-	/**
-	 * Polifil - float an array
-	 */
-	// if (!Array.prototype.flat) Array.prototype.flat = function () {
-	// 	return (function f(arr) {
-	// 	return arr.reduce(
-	// 			(a, v) =>
-	// 	Array.isArray(v)
-	// 				? a.concat(f(v))
-	// 				: a.concat(  v )
-	// 			, []
-	// 		)
-	// 	})(this)
-	// };
 
-	/**
-	 * Bounce animate for button
-	 */
-	// function createRipple(e) {
-	// 	const button = e.target.closest('.g-btn');
-	// 	const ripple = document.querySelectorAll(".ripple");
-	// 	if (ripple) ripple.forEach( el => el.remove() )
-	// 	const circle = document.createElement("span");
-	// 	const diameter = Math.max(button.clientWidth, button.clientHeight);
-	// 	const radius = diameter / 2;
-	// 	circle.style.width = circle.style.height = `${diameter}px`;
-	// 	circle.style.left = `${e.offsetX - radius }px`;
-	// 	circle.style.top = `${e.offsetY - radius }px`;
-	// 	circle.classList.add("ripple");
-	// 	button.appendChild(circle);
-	// }
-	// const buttons = document.querySelectorAll('.g-btn');
-	// if(buttons.length) buttons.forEach( el => el.addEventListener('click', createRipple) )
+	const calcParent = document.querySelector('.calc')
+	const currentTotalPrice = document.querySelector('.current-price')
+	const nextTotalBuyPriceElem = document.querySelector('.next-price')
+	const calcInputs = document.querySelectorAll('[data-calc]')
 
+	const formatPrice = new Intl.NumberFormat('ru-RU')
+	const regEXPBanString = /[^\d]/g
+	const regExpCountStr = /(^0+)|(\W+$)/gmi
+	const maxCount = 200
 
+	const PRICE_LIST = {
+		BOTTLE_PRICE: 400,
+		COOLER_PRICE: 4000,
+		NEXT_BUY_BOTTLE_PRICE: 500,
+	};
+	
+	const _priceInvoice = ( inputs = [] ) => {
+		let _total = Array.from(inputs).reduce( ( acc, inp ) => {
+			const typePrice = inp.dataset?.['type']
+			return acc += inp?.value.replace(regExpCountStr, '') * PRICE_LIST[typePrice]
+		}, 0)
+
+		return formatPrice.format(_total)
+	}
+	
+	function writeTotalPrice( inp, inputs ) {
+		const _countBottleElem = document.querySelector(`[data-type="BOTTLE_PRICE"]`)
+		inp.value = inp.value.replace(regEXPBanString, '');
+		inp.value >= maxCount ? inp.value = maxCount : true
+
+		currentTotalPrice.innerHTML = _priceInvoice(inputs)
+		
+		const _totalNextPrice = _countBottleElem?.value.replace(regExpCountStr, '') * PRICE_LIST['NEXT_BUY_BOTTLE_PRICE']
+		nextTotalBuyPriceElem.innerHTML = formatPrice.format(_totalNextPrice)
+		inp.value += ' шт'
+	}
+
+	calcParent?.addEventListener('input', (e) => writeTotalPrice(e.target, calcInputs))
+
+	
 })
